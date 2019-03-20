7092315d935b9f815b723b9dd492bd24bdfca5e8
Jhonattan <jma200@hotmail.com>
2019-03-20 01:06:12 -0500

    http server

---
+++ http_example1/http_example.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	// change the output field to be "message"
+	Message string `json:"message"`
+	// do not output this field
+	Author string `json:"-"`
+	// do not output the field if the value is empty
+	Date string `json:",omitempty"`
+	// convert output to a string and rename "id"
+	Id int `json:"id,string"`
+}
+
+func main() {
+	port := 8080
+	http.HandleFunc("/hello-world", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	fmt.Printf(":%v", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "HelloWorld"}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Ooops")
+	}
+
+	fmt.Fprint(w, string(data))
+}
